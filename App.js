8706cd458e6d644009af931bed2bc9ed1096d0d6
davyengone <davy@hackages.io>
2020-01-14 10:46:37 +0100

    starting point

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <View>
+        <Text>T</Text>
+        <Text>o</Text>
+        <Text>m</Text>
+        <Text>m</Text>
+        <Text>y</Text>
+      </View>
+      <View>
+        <View></View>
+        <View></View>
+      </View>
+      <View>
+        <Text>hilfiger</Text>
+      </View>
+    </View>
+  );
+}
+
+// import React from 'react';
+// import { View, Text } from 'react-native';
+
+// const App = () => {
+//   return (
+//     <View>
+//       <View style={{ backgroundColor: 'red', flex: 1 }}>
+//         <Text>This is a test</Text>
+//       </View>
+//       <View style={{ backgroundColor: 'blue', flex: 1 }}>
+//         <Text>This is a test</Text>
+//       </View>
+//       <View style={{ backgroundColor: 'green', flex: 1 }}>
+//         <Text>This is a test</Text>
+//       </View>
+//     </View>
+//   );
+// };
+// export default App;
